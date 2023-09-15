071118cf83c7b513c9d6679ca40b34e6ffa506b1
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2023-09-15 11:40:26 +0200

    feat: migrate to single node cluster

--- src/model/config/proxmox.ts
+++ src/model/config/proxmox.ts
@@ -1,11 +1,9 @@
-import { StringMap } from '../map';
-
 /**
  * Defines configuration data for Proxmox VE.
  */
 export type ProxmoxConfig = {
   readonly imageName: string;
-  readonly storagePool: StringMap<string>;
+  readonly storagePool: string;
   readonly localStoragePool: string;
   readonly networkBridge: string;
   readonly cpuType: string;
