1128897d45128e71157b057b6c31dbf9bb36c01f
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2025-07-22 13:36:04 +0200

    feat: remove aws firehose and athena

--- src/lib/configuration.ts
+++ src/lib/configuration.ts
@@ -1,7 +1,6 @@
 import { Config, getStack } from '@pulumi/pulumi';
 
 import { GoogleConfig } from '../model/config/google';
-import { HomeAssistantConfig } from '../model/config/home_assistant';
 import { NetworkConfig } from '../model/config/network';
 import { SecretStoresConfig } from '../model/config/secret_stores';
 import { TalosConfig } from '../model/config/talos';
@@ -12,8 +11,6 @@ const config = new Config();
 export const googleConfig = config.requireObject<GoogleConfig>('google');
 export const talosConfig = config.requireObject<TalosConfig>('talos');
 export const networkConfig = config.requireObject<NetworkConfig>('network');
-export const homeAssistantConfig =
-  config.requireObject<HomeAssistantConfig>('homeAssistant');
 export const secretStoresConfig =
   config.requireObject<SecretStoresConfig>('secretStores');
 export const bucketId = config.require<string>('bucketId');
--- src/lib/home_assistant/index.ts
+++ src/lib/home_assistant/index.ts
@@ -1,9 +1,4 @@
-import { createAthenaWorkgroup } from './athena';
-import { createFirehose } from './firehose';
-import { createGlueDatabase } from './glue';
 import { createGCPKey, createGCSKey } from './google';
-import { createGrafanaAWSAccessKey } from './grafana';
-import { createTelegrafAWSAccessKey } from './telegraf';
 
 /**
  * Creates the Home Assistant resources.
@@ -11,11 +6,4 @@ import { createTelegrafAWSAccessKey } from './telegraf';
 export const createHomeAssistantResources = () => {
   const iam = createGCPKey();
   createGCSKey(iam);
-
-  const firehoseDeliveryStreamArn = createFirehose();
-  createTelegrafAWSAccessKey(firehoseDeliveryStreamArn);
-
-  const glueDatabaseArn = createGlueDatabase();
-  const athenaWorkgroup = createAthenaWorkgroup();
-  createGrafanaAWSAccessKey(athenaWorkgroup, glueDatabaseArn);
 };
