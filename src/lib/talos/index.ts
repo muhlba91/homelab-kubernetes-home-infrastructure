91dede27234dfe7c4035f91fbbf80efac576602b
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2025-07-28 15:22:39 +0200

    feat: generate talosconfig all the time to avoid expiration

--- src/lib/talos/index.ts
+++ src/lib/talos/index.ts
@@ -1,5 +1,7 @@
+import { Command } from '@pulumi/command/local';
 import { all, Output } from '@pulumi/pulumi';
 import * as talos from '@pulumiverse/talos';
+import * as yaml from 'yaml';
 
 import { ClusterData } from '../../model/cluster';
 import { globalName, networkConfig, talosConfig } from '../configuration';
@@ -20,18 +22,13 @@ export const createCluster = (): ClusterData => {
     `talos-secrets-${talosConfig.cluster.revision}`,
     {},
   );
-  const talosClientConfig = talos.client.getConfigurationOutput({
-    clusterName: `${globalName}-cluster`,
-    clientConfiguration: talosSecrets.clientConfiguration,
-    nodes: [talosConfig.machine.network.ip.v4],
-    endpoints: [talosConfig.machine.network.ip.v4],
-  });
-
-  // write output files for the cluster
-  const controlplane = writeConfigFiles(
+  const controlplane = writeControlplaneAndSecretsFiles(
     talosSecrets.machineSecrets,
-    talosClientConfig.talosConfig,
   );
+
+  // Talos config files
+  const configFiles = writeTalosConfigFiles();
+  const talosconfigFile = configFiles[0];
 
   // install Talos & Kubernetes
   const installResource = installCluster();
@@ -60,20 +57,18 @@ export const createCluster = (): ClusterData => {
     resources: [installResource, ...upgradeResources],
     clientConfiguration: talosSecrets.clientConfiguration,
     kubeconfig: talosctlKubeConfig.kubeconfigRaw,
-    talosconfig: talosClientConfig.talosConfig,
+    talosconfig: talosconfigFile,
   };
 };
 
 /**
- * Writes the config files.
+ * Writes the controlplane and machine secrets files.
  *
  * @param {Output<talos.types.output.machine.MachineSecrets>} machineSecrets the machine secrets
- * @param {Output<string>} talosClientConfig the Talos client config
  * @returns {Output<unknown>} the output when the files are written
  */
-const writeConfigFiles = (
+const writeControlplaneAndSecretsFiles = (
   machineSecrets: Output<talos.types.output.machine.MachineSecrets>,
-  talosClientConfig: Output<string>,
 ): Output<unknown> => {
   const controlplane = writeFilePulumiAndUploadToS3(
     'controlplane.yml',
@@ -87,13 +82,57 @@ const writeConfigFiles = (
     ),
     {},
   );
+
   writeFilePulumiAndUploadToS3(
-    'talosconfig',
-    talosClientConfig.apply((config) =>
-      writeFileContents('./outputs/talosconfig', config, {}),
+    'secrets.yaml',
+    machineSecrets.apply((secrets) =>
+      yaml.stringify(structuredClone(secrets)).replace(/cert:/g, 'crt:'),
     ),
     {},
   );
 
   return controlplane;
+};
+
+/**
+ * Writes the talosconfig and kubeconfig file.
+ *
+ * @returns {Output<string[]>} the output containing the talosconfig and kubeconfig files
+ */
+const writeTalosConfigFiles = (): Output<string[]> => {
+  const configFiles = new Command(
+    'talos-config-files',
+    {
+      create: renderTemplate('assets/talos/talosconfig.sh.j2', {
+        endpoint: talosConfig.machine.network.ip.v4,
+      }),
+      update: renderTemplate('assets/talos/talosconfig.sh.j2', {
+        endpoint: talosConfig.machine.network.ip.v4,
+      }),
+      triggers: [Math.random()],
+    },
+    {
+      additionalSecretOutputs: ['stdout'],
+    },
+  );
+
+  const files = configFiles.stdout.apply((output) => {
+    const files = output.split('---FILE---');
+    writeFilePulumiAndUploadToS3(
+      'talosconfig',
+      Output.create(
+        writeFileContents('./outputs/talosconfig', files[1].trim(), {}),
+      ),
+      {},
+    );
+    // writeFilePulumiAndUploadToS3(
+    //   'admin.conf',
+    //   Output.create(writeFileContents('./outputs/admin.conf', files[2].trim(), {})),
+    //   {},
+    // );
+
+    return [files[1].trim(), files[2].trim()];
+  });
+
+  return files;
 };
