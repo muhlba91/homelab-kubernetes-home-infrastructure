be4a48d6c06580b1766ecdc278e065169a76096c
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2025-11-23 20:52:25 +0100

    feat: add cidrs to cilium

--- src/lib/cilium/index.ts
+++ src/lib/cilium/index.ts
@@ -1,9 +1,10 @@
 import { local } from '@pulumi/command';
-import { CustomResourceOptions } from '@pulumi/pulumi';
+import { CustomResourceOptions, Output } from '@pulumi/pulumi';
 
-import { environment } from '../configuration';
+import { environment, talosConfig } from '../configuration';
+import { writeFilePulumiAndUploadToS3 } from '../util/storage';
+import { renderTemplate } from '../util/template';
 
-// FIXME: templateize cilium.yml to use network config
 /**
  * Deploy cilium.
  *
@@ -14,8 +15,18 @@ export const deployCilium = ({
   pulumiOptions,
 }: {
   readonly pulumiOptions?: CustomResourceOptions;
-}): local.Command =>
-  new local.Command(
+}): local.Command => {
+  const values = writeFilePulumiAndUploadToS3(
+    'cilium_values.yml',
+    Output.create(
+      renderTemplate('assets/helm/cilium.yml.j2', {
+        network: talosConfig.cluster.network,
+      }),
+    ),
+    {},
+  );
+
+  return new local.Command(
     'helm-cilium',
     {
       create: './assets/helm/install.sh',
@@ -23,10 +34,12 @@ export const deployCilium = ({
         ENVIRONMENT: environment,
         DEPLOYMENT_ID: 'cilium',
         DEPLOYMENT_NAMESPACE: 'cilium',
-        VALUES_FILE: './assets/helm/cilium.yml',
+        VALUES_FILE: `./outputs/${environment}/cilium_values.yml`,
         HELM_REPO: 'https://helm.cilium.io/',
         HELM_CHART_NAME: 'cilium',
       },
+      triggers: [values],
     },
     pulumiOptions,
   );
+};
