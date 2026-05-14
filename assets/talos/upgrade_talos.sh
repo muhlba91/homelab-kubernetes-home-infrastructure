5a6ec96b9d96715fdfd75f680667ec854619e640
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2026-05-14 10:33:02 +0200

    feat: upgrade talos and kubernetes

--- assets/talos/upgrade_talos.sh
+++ assets/talos/upgrade_talos.sh
@@ -6,5 +6,5 @@ set -e
 while [ ! -f ./outputs/${ENVIRONMENT}/talosconfig ]; do sleep 5; done
 
 # upgrade talos
-TALOSCONFIG="./outputs/${ENVIRONMENT}/talosconfig" talosctl upgrade --nodes ${CONTROL_PLANE_IP} --image factory.talos.dev/installer/${INSTALL_IMAGE_HASH}:${TALOS_VERSION} --preserve --timeout=1h0m0s --force
+TALOSCONFIG="./outputs/${ENVIRONMENT}/talosconfig" talosctl upgrade --nodes ${CONTROL_PLANE_IP} --image factory.talos.dev/metal-installer/${INSTALL_IMAGE_HASH}:${TALOS_VERSION} --timeout=1h0m0s --reboot-mode=force
 sleep 300
